cba049557ef5ed55a7589c1acfa67d60b82bb4a5
michaelmorks <s224437209@deakin.edu.au>
2026-02-04 17:49:08 +1100

    Initial commit — push all project files

---
+++ java.js
@@ -0,0 +1,46 @@
+
+//dropdown meanu
+const hamburger = document.querySelector(".hamburger");
+const dropdown = document.getElementById("dropdownMenu");
+
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
+  dropdown.classList.toggle("active");
+});
+
+document.addEventListener("click", () => {
+  dropdown.classList.remove("active");
+});
+////////////////-------------
+
+
+// Auto silder
+window.addEventListener("load", () => {
+
+  const track = document.querySelector(".gallery-track");
+  const slides = Array.from(track.children);
+  
+  // Duplicate slides for infinite loop
+  slides.forEach(slide => {
+    const clone = slide.cloneNode(true);
+    track.appendChild(clone);
+  });
+  
+  let scrollPos = 0;
+  let speed = 1.3;
+  
+  function autoScroll() {
+    scrollPos += speed;
+    track.style.transform = `translateX(${-scrollPos}px)`;
+  
+    // Reset smoothly when half reached
+    if (scrollPos >= track.scrollWidth / 2) {
+      scrollPos = 0;
+    }
+  
+    requestAnimationFrame(autoScroll);
+  }
+  
+  autoScroll();
+  });
+  ////-------------------
